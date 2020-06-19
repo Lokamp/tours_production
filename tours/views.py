1a55d0ce9ff42ff043cb4b0d47b449902dc1cc0a
Evgen <sokolov.tt@gmail.com>
2020-06-19 20:56:13 +0300

    add href main page - test

--- tours/views.py
+++ tours/views.py
@@ -60,8 +60,8 @@ class TourView(View):
 
 
 def custom_handler404(request, exception):
-    return HttpResponseNotFound('Ошибка 404! Попробуйте открыть другую страницу')
+    return HttpResponseNotFound('Ошибка 404! Попробуйте открыть другую страницу. Главная href="/"')
 
 
 def custom_handler500(request):
-    return HttpResponseServerError('Ошибка 500! Попробуйте открыть другую страницу')
+    return HttpResponseServerError('Ошибка 500! Попробуйте открыть другую страницу. Главная href="/"')
